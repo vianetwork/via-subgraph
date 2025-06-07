7a00a8f362fc013c2394e37194e363ebdfacbda6
0xatomFusion <179967211+0xatomFusion@users.noreply.github.com>
2025-06-07 22:08:07 +0100

    initialize project

---
+++ src/l2BaseToken.ts
@@ -0,0 +1,54 @@
+import {
+  Mint as MintEvent,
+  Transfer as TransferEvent,
+  Withdrawal as WithdrawalEvent,
+} from "../generated/contracts-preprocessed/L2BaseToken.sol:L2BaseToken/contracts_preprocessed_L2BaseToken_sol_L2BaseToken"
+import {
+  Mint,
+  Transfer,
+  Withdrawal,
+} from "../generated/schema"
+
+export function handleMint(event: MintEvent): void {
+  let entity = new Mint(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.account = event.params.account
+  entity.amount = event.params.amount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.value = event.params.value
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleWithdrawal(event: WithdrawalEvent): void {
+  let entity = new Withdrawal(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity._l2Sender = event.params._l2Sender
+  entity._l1Receiver = event.params._l1Receiver
+  entity._amount = event.params._amount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
