c3be39fac4c437acd37cd362e9578c291d78edc7
petarTxFusion <petar@txfusion.io>
2025-11-30 20:16:19 +0100

    feat: update events

--- subgraphs/l1-monitor/src/mapping.ts
+++ subgraphs/l1-monitor/src/mapping.ts
@@ -1,18 +1,20 @@
-import { Deposit as DepositEvent, SyncFromL1 as SyncFromL1Event } from "../generated/Contract/Contract"
-import { Deposit, SyncFromL1 } from "../generated/schema"
+import { DepositMessageSent as DepositMessageSentEvent, MessageWithdrawalExecuted as MessageWithdrawalExecutedEvent } from "../generated/Contract/Contract"
+import { DepositMessageSent, MessageWithdrawalExecuted } from "../generated/schema"
 
-export function handleDeposit(event: DepositEvent): void {
-    let entity = new Deposit(event.transaction.hash.concatI32(event.logIndex.toI32()).toHexString())
-    entity.totalTVL = event.params.totalTVL
-    entity.sender = event.params.sender
-    entity.assets = event.params.assets
-    entity.receiver = event.params.receiver
+export function handleDepositMessageSent(event: DepositMessageSentEvent): void {
+    let entity = new DepositMessageSent(event.transaction.hash.concatI32(event.logIndex.toI32()).toHexString())
     entity.nonce = event.params.nonce
+    entity.l1Vault = event.params.l1Vault
+    entity.receiver = event.params.receiver
+    entity.shares = event.params.shares
     entity.save()
 }
 
-export function handleSyncFromL1(event: SyncFromL1Event): void {
-    let entity = new SyncFromL1(event.transaction.hash.concatI32(event.logIndex.toI32()).toHexString())
-    entity.totalTvl = event.params.totalTvl
+export function handleMessageWithdrawalExecuted(event: MessageWithdrawalExecutedEvent): void {
+    let entity = new MessageWithdrawalExecuted(event.transaction.hash.concatI32(event.logIndex.toI32()).toHexString())
+    entity.nonce = event.params.nonce
+    entity.l1Vault = event.params.l1Vault
+    entity.receiver = event.params.receiver
+    entity.shares = event.params.shares
     entity.save()
 }
