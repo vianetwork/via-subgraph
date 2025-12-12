81d8af48585edaa69b9e18e4c308ebeca83f0232
petarTxFusion <petar@txfusion.io>
2025-12-12 03:42:55 +0100

    feat: add monitoring for `WithdrawalStateUpdated`

--- subgraphs/eth-monitor/src/mapping.ts
+++ subgraphs/eth-monitor/src/mapping.ts
@@ -1,5 +1,6 @@
 import { DepositMessageSent as DepositMessageSentEvent, MessageWithdrawalExecuted as MessageWithdrawalExecutedEvent, MessageSent as MessageSentEvent } from "../generated/EthBridge/EthBridge"
-import { DepositMessageSent, MessageWithdrawalExecuted, MessageSent } from "../generated/schema"
+import { WithdrawalStateUpdated as WithdrawalStateUpdatedEvent } from "../generated/WithdrawalStateMonitor/WithdrawalStateMonitor"
+import { DepositMessageSent, MessageWithdrawalExecuted, MessageSent, WithdrawalStateUpdated } from "../generated/schema"
 
 export function handleDepositMessageSent(event: DepositMessageSentEvent): void {
     const id = event.transaction.hash.concatI32(event.logIndex.toI32()).toHexString()
@@ -50,6 +51,24 @@ export function handleMessageSent(event: MessageSentEvent): void {
 
     entity = new MessageSent(id)
     entity.payload = event.params.payload
+    entity.blockNumber = event.block.number
+    entity.blockTimestamp = event.block.timestamp
+    entity.transactionHash = event.transaction.hash
+    entity.save()
+}
+
+export function handleWithdrawalStateUpdated(event: WithdrawalStateUpdatedEvent): void {
+    const id = event.transaction.hash.concatI32(event.logIndex.toI32()).toHexString()
+
+    let entity = WithdrawalStateUpdated.load(id)
+    if (entity) {
+        return
+    }
+
+    entity = new WithdrawalStateUpdated(id)
+    entity.l1Batch = event.params.l1Batch
+    entity.exchangeRate = event.params.exchangeRate
+    entity.messageCount = event.params.messageCount
     entity.blockNumber = event.block.number
     entity.blockTimestamp = event.block.timestamp
     entity.transactionHash = event.transaction.hash
